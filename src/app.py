5c8ad6e661a671ff927f4abd5c2e824d6b74017a
yamin wang <miss77min@gmail.com>
2024-05-14 13:43:55 -0700

    Third Commit

--- src/app.py
+++ src/app.py
@@ -21,16 +21,3 @@ def index():
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-def main():
-    return '''
-     <form action="/echo_user_input" method="POST">
-         <input name="user_input">
-         <input type="submit" value="Submit!">
-     </form>
-     '''
-
-@app.route("/echo_user_input", methods=["POST"])
-def echo_input():
-    input_text = request.form.get("user_input", "")
-    return "You entered: " + input_text
