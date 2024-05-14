5fd3c63bfba69e3b97f34afd9d0f85c0ae76c3eb
yamin wang <miss77min@gmail.com>
2024-05-14 15:14:23 -0700

    Fifth Commit

--- src/app.py
+++ src/app.py
@@ -14,8 +14,77 @@ flashcards = [
 ]
 
 @app.route("/")
-def index():
-    return render_template("index.html", flashcards=flashcards)
+def main():
+    return """
+        <!DOCTYPE html>
+        <html>
+        <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <style>
+        body {
+        font-family: Arial, Helvetica, sans-serif;
+        }
 
-if __name__ == "__main__":
-    app.run(debug=True)
+        .flip-box {
+        background-color: transparent;
+        width: 200px;
+        height: 100px;
+        border: 1px solid #f1f1f1;
+        perspective: 1000px;
+        }
+
+        .flip-box-inner {
+        position: relative;
+        width: 100%;
+        height: 100%;
+        text-align: center;
+        transition: transform 0.8s;
+        transform-style: preserve-3d;
+        }
+
+        .flip-box:hover .flip-box-inner {
+        transform: rotateY(180deg);
+        }
+
+        .flip-box-front, .flip-box-back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        -webkit-backface-visibility: hidden;
+        backface-visibility: hidden;
+        }
+
+        .flip-box-front {
+        background-color: #bbb;
+        color: Black;
+        }
+
+        .flip-box-back {
+        background-color: dodgerblue;
+        color: white;
+        transform: rotateY(180deg);
+        }
+        </style>
+        </head>
+        <body>
+
+        <h1>Flashcards</h1>
+        <h3>Hover over the box below to see the translation in English:</h3>
+
+        <div class="flip-box">
+        <div class="flip-box-inner">
+            <div class="flip-box-front">
+            <h2>你好</h2>
+            </div>
+            <div class="flip-box-back">
+            <h2>Hello</h2>
+            </div>
+        </div>
+        </div>
+
+        </body>
+        </html>
+
+
+    """
+
